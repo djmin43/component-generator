ad2c614106d6c07c14c7bf2143446b63450e3cbc
jay <dj.min43@gmail.com>
2021-11-14 13:50:41 +0900

    add buttons

--- components/control-bar/ControlBar.tsx
+++ components/control-bar/ControlBar.tsx
@@ -4,6 +4,15 @@ const ControlBar = () => {
   return (
     <div>
       i am controller
+      <button>
+        add component
+      </button>
+      <button>
+        edit component
+      </button>
+      <button>
+        delete component
+      </button>
     </div>
   )
 }
